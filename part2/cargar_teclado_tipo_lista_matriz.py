b6eb38f7d3925283a4410bf229db373d34d3e5c7
cuervoz12 <xxxocampocuervo321xxx@gmail.com>
2026-08-02 22:10:45 -0500

    Ejercicio de carga de datos para matrices paralelas

--- part2/cargar_teclado_tipo_lista_matriz.py
+++ part2/cargar_teclado_tipo_lista_matriz.py
@@ -18,4 +18,51 @@ for x in range (3):
     
 """
 
-    
+"""
+Se tiene que cargar la siguiente información:
+· Nombres de 3 empleados
+· Ingresos en concepto de sueldo, cobrado por cada empleado, en los últimos 3 meses.
+Confeccionar el programa para:
+
+a) Realizar la carga de los nombres de empleados y los tres sueldos por cada empleado.
+b) Generar una lista que contenga el ingreso acumulado en sueldos en los últimos 3 meses para cada empleado.
+c) Mostrar por pantalla el total pagado en sueldos a cada empleado en los últimos 3 meses
+d) Obtener el nombre del empleado que tuvo el mayor ingreso acumulado
+
+
+nombre = []
+sueldos = []
+totalsueldos = []
+
+for x in range (3):
+
+    a = input("Ingresa el nombre del empleado: ")
+    nombre.append(a)
+    s1 = int(input("Ingresa el sueldo 1: "))
+    s2 = int(input("Ingresa el sueldo 2: "))
+    s3 = int(input("Ingresa el sueldo 3: "))
+    sueldos.append([s1, s2, s3])
+
+for x in range (3):
+
+    total = sueldos[x][0] + sueldos[x][1] + sueldos[x][2]
+    totalsueldos.append(total)
+
+for x in range (3):
+
+    print("Empleado: ", nombre[x], " Sueldos en los ultimos 3 meses: ", totalsueldos[x])
+
+mayorn = nombre[0]
+mayors = totalsueldos[0]
+
+for x in range (1, 3):
+
+    if totalsueldos[x] > mayors:
+        mayors = nombre[x]
+        mayors = x
+
+print("El empleado con mayor ingreso es: ", mayorn, " con el sueldo: ", mayors) 
+
+"""
+
+
