1f1f641a2ad7afe1e143f0996f20da6f196ba37b
cuervoz12 <xxxocampocuervo321xxx@gmail.com>
2026-09-01 21:42:43 -0500

    Clases

--- part3/clase_objetos.py
+++ part3/clase_objetos.py
@@ -2,7 +2,6 @@
 Implementaremos una clase llamada Persona que tendrá como atributo (variable) su nombre y dos métodos (funciones), uno de dichos métodos inicializará el atributo nombre y el siguiente método mostrará en la pantalla el contenido del mismo.
 Definir dos objetos de la clase Persona.
 
-"""
 
 class persona:
 
@@ -21,3 +20,43 @@ persona1.imprimir ()
 persona2 = persona ()
 persona2.inicializar ("Carla")
 persona2.imprimir ()
+
+"""
+
+"""
+Implementar una clase llamada Alumno que tenga como atributos su nombre y su nota. Definir los métodos para inicializar sus atributos, imprimirlos y mostrar un mensaje si está regular (nota mayor o igual a 4)
+
+Definir dos objetos de la clase Alumno.
+
+class Alumno:
+
+    def inicializar (self, nombre, nota):
+
+        self.nombre = nombre
+        self.nota = nota
+
+    def imprimir (self):
+
+        print("Nombre: ", self.nombre)
+        print("Nota: ", self.nota)
+
+    def mostrar_estado (self):
+
+        if self.nota >= 4:
+            print("Regular")
+        else:
+            print("Libre")
+
+alumno1 = Alumno ()
+alumno1.inicializar ("Diego", 2)
+alumno1.imprimir ()
+alumno1.mostrar_estado ()
+
+alumno2 = Alumno ()
+alumno2.inicializar ("Ana", 10)
+alumno2.imprimir ()
+alumno2.mostrar_estado ()
+
+"""
+
+
